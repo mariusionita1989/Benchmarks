1562beb05f55d15f73281442f024c19b655d07d5
Marius <marius.ionita.portfolio@outlook.com>
2023-09-13 04:00:45 +0300

    Add some extrabenchmarks for string comparison and object serialization and deserialization

--- Benchmarks/Benchmarks/StringOperationsDemo.cs
+++ Benchmarks/Benchmarks/StringOperationsDemo.cs
@@ -12,17 +12,39 @@ namespace Benchmarks.Benchmarks
     {
         private const int length = 1024 * 1024;
         private char[] input = null;
+        private string a = string.Empty;
+        private string b = string.Empty;
 
         [GlobalSetup]
         public void GlobalSetup()
         {
             input = RandomStringGenerator.GenerateRandomString(length).ToCharArray();
+            a = RandomStringGenerator.GenerateRandomString(length);
+            b = RandomStringGenerator.GenerateRandomString(length);
         }
 
         [Benchmark]
         public void GetCharsCount()
         {
             Counter.CharsCount(input);
+        }
+
+        [Benchmark]
+        public void EqualOperatorStringCompare()
+        {
+            Comparison.EqualOperatorComparison(a,b);
+        }
+
+        [Benchmark]
+        public void EqualsStringCompare()
+        {
+            Comparison.EqualsComparison(a,b);
+        }
+
+        [Benchmark]
+        public void StringCompare()
+        {
+            Comparison.StringCompare(a,b);
         }
     }
 }
---
+++ Benchmarks/Serialization/NugetPerson.cs
@@ -0,0 +1,14 @@
+﻿using MessagePack;
+
+namespace Benchmarks.Serialization
+{
+    [MessagePackObject]
+    public class NugetPerson
+    {
+        [Key(0)]
+        public string? Name { get; set; }
+
+        [Key(1)]
+        public int Age { get; set; }
+    }
+}
